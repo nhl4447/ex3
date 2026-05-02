ebcda714b8fcbdcf683f65889acd63919ec1064a
Lam Nguyen <pronhl4447@gmail.com>
2026-05-02 13:48:36 +0700

    Add project files.

---
+++ ex3/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace ex3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Distance (m): ");
+            double d = double.Parse(Console.ReadLine());
+
+            Console.Write("Hours: ");
+            int h = int.Parse(Console.ReadLine());
+
+            Console.Write("Minutes: ");
+            int m = int.Parse(Console.ReadLine());
+
+            Console.Write("Seconds: ");
+            int s = int.Parse(Console.ReadLine());
+
+            double t = h * 3600 + m * 60 + s;
+
+            Console.WriteLine("Km/h = " + d * 3.6 / t);
+            Console.WriteLine("Miles/h = " + d * 2.23694 / t);
+        }
+    }
+}
